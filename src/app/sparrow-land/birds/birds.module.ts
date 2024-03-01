6b70f0d483dfa64ddb49861a76332270c221dd7b
Deep5656 <deepanshukatiyar5656@gmail.com>
2024-03-01 12:38:29 +0530

    birds home page modified

--- src/app/sparrow-land/birds/birds.module.ts
+++ src/app/sparrow-land/birds/birds.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BirdHomeComponent } from './bird-home/bird-home.component';
 import { RouterModule, Routes } from '@angular/router';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
 
 const birdRoute:Routes = [
   {path:'',component:BirdHomeComponent}
@@ -13,6 +15,8 @@ const birdRoute:Routes = [
   ],
   imports: [
     CommonModule,
+    MatCardModule,
+    MatInputModule,
     RouterModule.forChild(birdRoute)
   ]
 })
